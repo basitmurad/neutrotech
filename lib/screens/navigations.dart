fdfcc722510c9f8ce26563ae61ee0ad15c0fa1fd
basit475 <basitmurad7@gmail.com>
2024-06-07 22:01:50 +0500

    changes

---
+++ lib/screens/navigations.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:neutrotech/screens/dashboard/MealScreen.dart';
+
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationCOntroller());
+    return Scaffold(
+      bottomNavigationBar: Obx(
+            () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+
+          onDestinationSelected: (index) =>
+          controller.selectedIndex.value = index,
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.cabin), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.cabin_outlined), label: 'booking'),
+            NavigationDestination(icon: Icon(Icons.cabin_outlined), label: 'Profile'),
+          ],
+        ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationCOntroller extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+     MealScreen(),
+    Container(
+      color: Colors.orange,
+    ),    Container(
+      color: Colors.orange,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
+}
